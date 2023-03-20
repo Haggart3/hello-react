292ef1009764e247ac146e9ddcbae0323f11b0ba
Haggart3 <haggart3@msu.edu>
2023-03-20 17:20:03 -0400

    test deploy commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Aidan and MSU, MI449!
         </a>
       </header>
     </div>
